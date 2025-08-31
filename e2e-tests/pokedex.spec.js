bd7fe417cab1c2077f9865c8cfe4975149cceec7
thetripleacoder <85655422+thetripleacoder@users.noreply.github.com>
2025-09-01 04:20:02 +0800

    Exercise 11.9 Simple end-to-end tests feat: Add Playwright end-to-end tests
    - Added Playwright config and workflow
    - Created e2e test for front page
    - Installed Playwright dependencies
    - Added test script in package.json

---
+++ e2e-tests/pokedex.spec.js
@@ -0,0 +1,11 @@
+const { test, expect } = require('@playwright/test');
+
+test('front page can be opened', async ({ page }) => {
+  await page.goto('http://localhost:5000');
+  await expect(page.getByText('ivysaur')).toBeVisible();
+  await expect(
+    page.getByText(
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    )
+  ).toBeVisible();
+});
