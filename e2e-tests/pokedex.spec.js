bb1edac64677d74b954983436c0d26ffaea20c67
thetripleacoder <85655422+thetripleacoder@users.noreply.github.com>
2025-09-01 05:00:35 +0800

    Exercise 11.9 Simple end-to-end tests test: update testing-library/jest-dom version
    - Updated @testing-library/jest-dom
    - Updated package-lock.json

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -1,11 +1,11 @@
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test')
 
 test('front page can be opened', async ({ page }) => {
-  await page.goto('http://localhost:5000');
-  await expect(page.getByText('ivysaur')).toBeVisible();
+  await page.goto('http://localhost:5000')
+  await expect(page.getByText('ivysaur')).toBeVisible()
   await expect(
     page.getByText(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
-  ).toBeVisible();
-});
+  ).toBeVisible()
+})
