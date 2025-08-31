4490a5745c5fc6111cb1aa72bf253b038bb31bc6
thetripleacoder <85655422+thetripleacoder@users.noreply.github.com>
2025-09-01 05:31:19 +0800

    Exercise 11.9 refactor: update Playwright config
    - Changed the web server command
    - Updated the server URL
    - Removed the port configuration
    - Added baseURL configuration

--- plawywright.config.js
+++ plawywright.config.js
@@ -1,8 +1,12 @@
-module.exports = {
+import { defineConfig } from '@playwright/test'
+export default defineConfig({
   webServer: {
-    command: 'npm run start-prod',
-    port: 5000,
+    command: 'npm run start',
+    url: 'http://localhost:3000',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
-}
+  use: {
+    baseURL: 'http://localhost:3000/',
+  },
+})
